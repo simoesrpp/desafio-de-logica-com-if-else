19fbf334bbff509673d65ac3d2d62f20e3d86b40
MarcosWilame <wilame_marcos@outlook.com>
2023-06-19 19:14:15 -0300

    dd

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const prompt = require('prompt-sync')();
 
-const name = prompt('What is your name?');
+const idade = prompt('Informe sua idade:');
+
+
+function verificaIdade(idade) {
+
+   
+if(idade<0.1){
+
+    console.log ('Idade inválida');
+    return;
+}
+
+if (idade<18){
+
+    console.log ('Você é menor de idade');
+    return;
+
+}
+
+if (idade>17){
+
+    console.log('Você é maior de idade')
+    
+} 
+
+}
+
+verificaIdade (idade)
+
+
